913a5d8de4673d3cef248770b409d8ecbad38e47
Jack Singleton <git@jacksingleton.com>
2015-01-12 17:42:35 -0800

    add main loop

--- injection/src/main/java/com/thoughtworks/securityinourdna/Main.java
+++ injection/src/main/java/com/thoughtworks/securityinourdna/Main.java
@@ -8,16 +8,18 @@ public class Main {
     private static final UserRepo userRepo = initializeUserRepo();
 
     public static void main(final String[] args) throws Exception {
-        System.out.print("Hi what is your first name? ");
+        while (true) {
+            System.out.print("Hi what is your first name? ");
 
-        final String firstName = new Scanner(System.in).nextLine();
+            final String firstName = new Scanner(System.in).nextLine();
 
-        final String lastName = userRepo.findLastName(firstName);
+            final String lastName = userRepo.findLastName(firstName);
 
-        if (lastName != null) {
-            System.out.println("Hi, " + firstName + " " + lastName);
-        } else {
-            System.out.println("Sorry, you're not in our database");
+            if (lastName != null) {
+                System.out.println("Hi, " + firstName + " " + lastName);
+            } else {
+                System.out.println("Sorry, you're not in our database");
+            }
         }
     }
 
