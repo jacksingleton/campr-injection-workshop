361c334323b64cd7f76b28fdda4cea2ab86deb6b
Jaydeepc <jaydeepc@in-Jaydeepc-192.local>
2019-10-16 00:37:49 +0530

    JD: Fixed table create issue

--- src/main/java/com/thoughtworks/securityinourdna/ConnectionFactory.java
+++ src/main/java/com/thoughtworks/securityinourdna/ConnectionFactory.java
@@ -11,12 +11,20 @@ public class ConnectionFactory {
     private final String usersTableSql = "create table users (vendor_name varchar(80), password varchar(80))";
 
     public Connection createInMemoryDatabase() throws SQLException {
+        int size =0;
         final Connection conn = DriverManager.getConnection(databaseDescriptor, "root","");
         boolean tableFound = false;
         DatabaseMetaData meta = conn.getMetaData();
         ResultSet res = meta.getTables(null, null, "users", new String[] {"TABLE"});
-
-        if (res == null){
+        if (res != null) 
+        {
+            res.last();
+            size = res.getRow();
+        }
+        System.out.println("****************");
+        System.out.println(size);
+        System.out.println("****************");
+        if (size == 0){
             conn.createStatement().execute(usersTableSql);   
         }       
          
